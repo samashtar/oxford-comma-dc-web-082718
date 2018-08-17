43da8bd263d041cb77ac184c02654195e6f68275
Sam Ashtar <samashtarm@gmail.com>
2018-08-17 22:39:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
-
-if array.size == 1, 
-  array.join
-elsif array.size == 2,
-  array.join ("and")
-
-end 
+  case array.size
+  when 1
+    array.join
+  when 2
+    return array.join(" and ")
+  else
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
 end

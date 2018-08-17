cc2fa712a2c05789b78a086a20453fbeb95a4e34
Sam Ashtar <samashtarm@gmail.com>
2018-08-17 22:42:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    array.join
+  when 2
+    return array.join(" and ")
+  else
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
 end
